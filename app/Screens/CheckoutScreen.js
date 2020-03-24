e80731c039b1cd6968f0752f83cd53dddba860c3
Da1man <chaos4ever@mail.ru>
2020-03-24 23:16:13 +0300

    add fields to checkout

--- app/Screens/CheckoutScreen.js
+++ app/Screens/CheckoutScreen.js
@@ -1,5 +1,7 @@
 import React, {PureComponent} from 'react';
-import {StyleSheet, Text, View, ScrollView, TouchableOpacity, TextInput, Keyboard} from 'react-native';
+import {StyleSheet, Text, View, ScrollView, TouchableOpacity, TextInput, Picker, Keyboard} from 'react-native';
+import RadioForm from 'react-native-simple-radio-button';
+
 import Header from '../components/Header';
 import {THEME, w} from '../common/variables';
 
@@ -11,7 +13,17 @@ class CheckoutScreen extends PureComponent {
   }
 
   render() {
-    const {navigation} = this.props;
+    const {navigation, cartProducts, cartTotal} = this.props;
+    const orderCartItems = cartProducts.map((item) => <View key={item.id} style={styles.cartOrderItem}>
+      <View>
+        <Text style={styles.orderItemsText}>{`${item.name} X ${item.count}`}</Text>
+      </View>
+      <View>
+        <Text style={styles.orderItemsText}>{`${item.discountPrice
+          ? item.discountPrice * item.count
+          : item.price * item.count} ₽`}</Text>
+      </View>
+    </View>);
 
     return (
       <View style={{flex: 1}}>
@@ -27,26 +39,96 @@ class CheckoutScreen extends PureComponent {
               style={styles.inputText}
               multiline={false}
               maxLength={50}
-              placeholder={'Ваше имя ...'}
+              placeholder={'Ваше имя'}
               placeholderTextColor={THEME.COLOR.GRAY}
             />
             <TextInput
               style={styles.inputText}
               multiline={false}
               maxLength={50}
-              placeholder={'Ваш телефон ...'}
+              placeholder={'Ваш телефон'}
               placeholderTextColor={THEME.COLOR.GRAY}
             />
             <TextInput
               style={styles.inputText}
               multiline={false}
               maxLength={50}
-              placeholder={'Ваш e-mail ...'}
+              placeholder={'Ваш e-mail'}
               placeholderTextColor={THEME.COLOR.GRAY}
             />
           </View>
           <View style={styles.titleSection}>
             <Text style={styles.titleText}>ДЕТАЛИ ДОСТАВКИ</Text>
+          </View>
+          <View style={styles.deliveryDetailsSection}>
+            <TextInput
+              style={styles.inputText}
+              multiline={false}
+              maxLength={200}
+              placeholder={'Адрес доставки'}
+              placeholderTextColor={THEME.COLOR.GRAY}
+            />
+            <View style={styles.inputDropdown}>
+              <Picker
+                selectedValue={'Кимры/Савелово. Мин заказ 500 ₽'}
+                style={{}}
+                itemStyle={{
+                  backgroundColor: 'red',
+                  padding: 10,
+                }}
+                itemTextStyle={{
+                  padding: 10,
+                  fontSize: THEME.FONT_SIZE.MAIN,
+                  fontFamily: THEME.FONT_FAMILY.REGULAR,
+                  backgroundColor: 'red',
+                }}
+                onValueChange={(itemValue) =>
+                  console.log(itemValue)
+                }>
+                <Picker.Item
+                  label={`Кимры/Савелово. Мин заказ 500 ₽`}
+                  value={`Кимры/Савелово. Мин заказ 500 ₽`}
+                />
+                <Picker.Item
+                  label={`Деревни/Сад. тов./Док. Мин заказ 1000 ₽`}
+                  value={`Деревни/Сад. тов./Док. Мин заказ 1000 ₽`}
+                />
+              </Picker>
+            </View>
+            <TextInput
+              style={styles.commentInput}
+              multiline={true}
+              maxLength={1000}
+              placeholder={`Примечания к вашему заказу, например, особые пожелания отделу доставки.`}
+              placeholderTextColor={THEME.COLOR.GRAY}
+            />
+          </View>
+          <View style={styles.titleSection}>
+            <Text style={styles.titleText}>ВАШ ЗАКАЗ</Text>
+          </View>
+          <View style={styles.cartSection}>
+            {orderCartItems}
+            <View style={styles.totalSection}>
+              <Text style={styles.totalText}>{`Итого: ${cartTotal} ₽`}</Text>
+            </View>
+          </View>
+          <View style={styles.paymentMethodSection}>
+            <RadioForm
+              radio_props={[
+                {label: 'Оплата курьеру наличными', value: 'nal'},
+                {label: 'Оплата курьеру через терминал', value: 'terminal'},
+              ]}
+              initial={-1}
+              onPress={(value) => {
+                console.log(value);
+              }}
+              buttonColor={THEME.COLOR.GRAY}
+              labelColor={THEME.COLOR.BLACK}
+              selectedButtonColor={THEME.COLOR.ACCENT}
+              labelStyle={{fontFamily: THEME.FONT_FAMILY.REGULAR, fontSize: THEME.FONT_SIZE.MAIN}}
+            >
+
+            </RadioForm>
           </View>
         </ScrollView>
       </View>
@@ -86,6 +168,55 @@ const styles = StyleSheet.create({
     fontSize: THEME.FONT_SIZE.MAIN,
     paddingHorizontal: 15,
   },
+  inputDropdown: {
+    borderWidth: 1,
+    borderColor: THEME.COLOR.GRAY_DARK,
+    borderRadius: 50,
+    marginBottom: 20,
+    paddingHorizontal: 15,
+  },
+  commentInput: {
+    borderWidth: 1,
+    borderColor: THEME.COLOR.GRAY_DARK,
+    borderRadius: 20,
+    marginBottom: 20,
+    fontFamily: THEME.FONT_FAMILY.REGULAR,
+    fontSize: THEME.FONT_SIZE.MAIN,
+    paddingHorizontal: 15,
+    height: 150,
+    textAlign: 'left',
+    textAlignVertical: 'top',
+  },
+  deliveryDetailsSection: {
+    paddingHorizontal: 15,
+  },
+  cartSection: {
+    marginBottom: 40,
+    paddingHorizontal: 30,
+  },
+  cartOrderItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  orderItemsSection: {},
+  orderItemsText: {
+    fontFamily: THEME.FONT_FAMILY.REGULAR,
+    fontSize: THEME.FONT_SIZE.MAIN,
+    color: THEME.COLOR.BLACK,
+  },
+  totalSection: {
+    alignItems: 'flex-end',
+  },
+  totalText: {
+    fontFamily: THEME.FONT_FAMILY.BOLD,
+    fontSize: THEME.FONT_SIZE.TITLE,
+    color: THEME.COLOR.BLACK,
+  },
+  paymentMethodSection: {
+    marginBottom: 40,
+    paddingHorizontal: 30,
+  },
 });
 
 let mapStateToProps = state => {
@@ -95,6 +226,4 @@ let mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, {
-
-})(CheckoutScreen);
+export default connect(mapStateToProps, {})(CheckoutScreen);
